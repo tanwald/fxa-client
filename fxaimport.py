fec59a199c7811ba5d0db78ece7591ea50d92865
Paul Neulinger <paul.neulinger@tanwald.net>
2019-06-10 20:03:12 +0200

    advanced display of invalid records;

--- fxaimport.py
+++ fxaimport.py
@@ -10,14 +10,21 @@ class FxaJsonImport(object):
         with open(data, mode='r') as records_file:
             records = json.load(records_file)
             for record in records['items']:
-                if record['folderId'] == '0f7b6962-94ac-4006-9a13-a86a00f828c2' and 'login' in record:
-                    original = record
-                    record = self.convert(record)
-                    if None not in record.values():
-                        converted_records.append(record)
+                if record['folderId'] == 'facda3dd-60c7-4226-9188-a7d90160935f':
+                    if 'login' in record:
+                        original = record
+                        if len(original.keys()) != 9:
+                            print('\nrecord with additional keys:')
+                            print(json.dumps(original, indent=2))
+                        record = self.convert(record)
+                        if None not in record.values():
+                            converted_records.append(record)
+                        else:
+                            print('\ninvalid record:')
+                            print(json.dumps(original, indent=2))
                     else:
                         print('\ninvalid record:')
-                        print(json.dumps(original, indent=2))
+                        print(json.dumps(record, indent=2))
 
         if dryrun:
             print(json.dumps(converted_records, indent=2))
@@ -26,7 +33,7 @@ class FxaJsonImport(object):
 
     def convert(self, record):
         hostname = None
-        if 'uris' in record['login']:
+        if 'uris' in record['login'] and record['login']['uris'] and len(record['login']['uris']) > 0:
             hostname = record['login']['uris'][0]['uri'].split('/')
             if (len(hostname)) > 1:
                 hostname = hostname[2]
